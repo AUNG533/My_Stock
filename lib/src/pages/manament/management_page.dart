82ac921930943684b707d997af18d7b9cf0cf729
aungkyawlin <aungkyawlinmon@gmail.com>
2022-06-04 12:01:40 +0700

    Product Form part 2

--- lib/src/pages/manament/management_page.dart
+++ lib/src/pages/manament/management_page.dart
@@ -1,7 +1,7 @@
 // management_page.dart
 // ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
+import 'package:mystock/src/models/product.dart';
 
 class ManagementPage extends StatefulWidget {
   const ManagementPage({Key? key}) : super(key: key);
@@ -13,20 +13,22 @@ class ManagementPage extends StatefulWidget {
 class _ManagementPageState extends State<ManagementPage> {
   late bool _isEdit;
   final _spacing = 12.0;
+  late Product _product;
+  final _form = GlobalKey<FormState>();
 
   @override
   void initState() {
     _isEdit = false;
+    _product = Product();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(_isEdit ? 'Edit Product' : 'Create Product'),
-      ),
+      appBar: _buildAppBar(), // ExtractMethod AppBar
       body: Form(
+        key: _form,
         child: Padding(
           padding: const EdgeInsets.all(12.0),
           child: Column(
@@ -51,6 +53,24 @@ class _ManagementPageState extends State<ManagementPage> {
     );
   }
 
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: Text(_isEdit ? 'Edit Product' : 'Create Product'),
+      actions: [
+        FlatButton(
+          textColor: Colors.white,
+          onPressed: () {
+            _form.currentState?.save();
+            print(_product.name);
+            print(_product.price);
+            print(_product.stock);
+          },
+          child: Text('submit'),
+        ),
+      ],
+    );
+  }
+
   InputDecoration inputStyle(label) => InputDecoration(
         focusedBorder: OutlineInputBorder(
           borderSide: BorderSide(
@@ -69,13 +89,24 @@ class _ManagementPageState extends State<ManagementPage> {
 
   TextFormField _buildNameInput() => TextFormField(
         decoration: inputStyle('name'),
+        onSaved: (String? value) {
+          _product.name = value!.isEmpty ? '-' : value;
+        },
       );
 
   TextFormField _buildPriceInput() => TextFormField(
         decoration: inputStyle('price'),
+        keyboardType: TextInputType.number,
+        onSaved: (String? value) {
+          _product.price = value!.isEmpty ? 0 : int.parse(value);
+        },
       );
 
   TextFormField _buildStockInput() => TextFormField(
         decoration: inputStyle('stock'),
+        keyboardType: TextInputType.number,
+        onSaved: (String? value) {
+          _product.stock = value!.isEmpty ? 0 : int.parse(value);
+        },
       );
 }
